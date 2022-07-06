94bd483720a0b5d0c65115a53bdd03a258392f29
jayropatti <jayro.pattimaipau@student.hu.nl>
2022-07-06 13:22:30 +0200

    commit snake info in BattlesnakeResource

--- src/main/webapp/snake/snake-service.js
+++ src/main/webapp/snake/snake-service.js
@@ -1,23 +1,25 @@
 export default class SnakeService {
     async getSnake() {
-
-        return fetch("restservices/snake")
-            .then(response => response.json())
-
-        return Promise.resolve({
-            apiversion: "1",
-            author: "Jayro Pattimaipau",
-            color: "#0000FF",
-            head: "replit-mark",
-            tail: "rbc-necktie",
-            version: "0.1"
-        });
-
-
+        return fetch("/restservices/snake").then(response => response.json())
+        //
+        // return Promise.resolve({
+        //     apiversion: "1",
+        //     author: "Jayro Pattimaipau",
+        //     color: "#0000FF",
+        //     head: "replit-mark",
+        //     tail: "rbc-necktie",
+        //     version: "0.1"
+        // });
+        // dit was overbodig
     }
 
     async updateSnake(updatedSnake) {
         //TODO: update je slang aan de server-kant met de nieuwe gegevens
-        return Promise.resolve();
+        let fetchOptions = {
+            method: "PUT",
+            body: JSON.stringify(updatedSnake),
+            headers: {"Content-type": "application/json"}
+        }
+        return fetch("/restservices/snake/snakeUpdate", fetchOptions).then(response => response.status)
     }
 }
